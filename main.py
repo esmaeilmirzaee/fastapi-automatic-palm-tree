536612824e0e86d8956ebc42fd5ed44c5c0e116b
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-06-17 23:42:26 +0430

    Create two users and fix a bug in our models

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from .models import User, Gender, Role
+from typing import List
+from uuid import uuid4
+
 
 app = FastAPI()
+
+db: List[User] = [
+    User(id=uuid4(), first_name='Jane', last_name='DOE', gender=Gender.female, roles=[Role.user, Role.admin]),
+    User(id=uuid4(), first_name='John', last_name='DOE', gender=Gender.female, roles=[Role.student])
+]
 
 
 @app.get('/')

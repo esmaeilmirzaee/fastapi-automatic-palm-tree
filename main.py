1f0cff9a918b6441a5fede5814a5095b058d29de
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-06-18 00:49:19 +0430

    Response if a user not found

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import User, Gender, Role
 from typing import List
 from uuid import UUID
 
-
 app = FastAPI()
 
 db: List[User] = [
-    User(id=UUID("522e2967-75e4-48e9-a922-6805ca248e66"), first_name='Jane', last_name='DOE', gender=Gender.female, roles=[Role.user, Role.admin]),
+    User(id=UUID("522e2967-75e4-48e9-a922-6805ca248e66"), first_name='Jane', last_name='DOE', gender=Gender.female,
+         roles=[Role.user, Role.admin]),
     # User(id=UUID("2140e1c7-cd57-4544-8858-b3f7bc87ac1f"), first_name='John', last_name='DOE', gender=Gender.female,
     # roles=[Role.student])
 ]
@@ -35,4 +35,5 @@ async def delete_user(user_id: UUID):
         if user.id == user_id:
             db.remove(user)
             return {"message": "User deleted"}
-    return {"message": "User not found"}
+
+    raise HTTPException(status_code=404, detail='User not found')

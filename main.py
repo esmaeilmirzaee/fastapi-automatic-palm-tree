bc3b0545faabdbb134ca0930785239e7a64ef39f
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-06-18 00:32:14 +0430

    Delete a user

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 
 db: List[User] = [
     User(id=UUID("522e2967-75e4-48e9-a922-6805ca248e66"), first_name='Jane', last_name='DOE', gender=Gender.female, roles=[Role.user, Role.admin]),
-    # User(id=UUID("2140e1c7-cd57-4544-8858-b3f7bc87ac1f"), first_name='John', last_name='DOE', gender=Gender.female, roles=[Role.student])
+    # User(id=UUID("2140e1c7-cd57-4544-8858-b3f7bc87ac1f"), first_name='John', last_name='DOE', gender=Gender.female,
+    # roles=[Role.student])
 ]
 
 
@@ -26,3 +27,12 @@ async def fetch_users():
 async def create_user(user: User):
     db.append(user)
     return {"message": "User created", 'user': user}
+
+
+@app.delete('/api/v1/users/{user_id}')
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return {"message": "User deleted"}
+    return {"message": "User not found"}

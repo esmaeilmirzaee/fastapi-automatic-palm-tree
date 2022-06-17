9b278c3f8dcb6fdc79e97c3bc3f3111523fd43d8
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-06-18 00:07:26 +0430

    Register a new user

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from models import User, Gender, Role
 from typing import List
-from uuid import uuid4
+from uuid import UUID
 
 
 app = FastAPI()
 
 db: List[User] = [
-    User(id=uuid4(), first_name='Jane', last_name='DOE', gender=Gender.female, roles=[Role.user, Role.admin]),
-    User(id=uuid4(), first_name='John', last_name='DOE', gender=Gender.female, roles=[Role.student])
+    User(id=UUID("522e2967-75e4-48e9-a922-6805ca248e66"), first_name='Jane', last_name='DOE', gender=Gender.female, roles=[Role.user, Role.admin]),
+    # User(id=UUID("2140e1c7-cd57-4544-8858-b3f7bc87ac1f"), first_name='John', last_name='DOE', gender=Gender.female, roles=[Role.student])
 ]
 
 
@@ -20,3 +20,9 @@ def root():
 @app.get('/api/v1/users')
 async def fetch_users():
     return db
+
+
+@app.post('/api/v1/users')
+async def create_user(user: User):
+    db.append(user)
+    return {"message": "User created", 'user': user}
